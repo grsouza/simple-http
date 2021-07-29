c53225cca1884ff0a1e276d99568b19d2dc52d04
Guilherme Souza <grsouza@pm.me>
2021-07-29 06:49:31 -0300

    swift format

--- Sources/SimpleHTTP/Endpoint.swift
+++ Sources/SimpleHTTP/Endpoint.swift
@@ -1,45 +1,48 @@
 import Foundation
 
 public struct Endpoint {
-    public let path: String
-    public let method: HTTPMethod
-    public let query: [URLQueryItem]?
-    public let headers: [String: String]
-    public let body: Data?
+  public let path: String
+  public let method: HTTPMethod
+  public let query: [URLQueryItem]?
+  public let headers: [String: String]
+  public let body: Data?
 
-    public init(
-        path: String,
-        method: HTTPMethod,
-        query: [URLQueryItem]?,
-        headers: [String: String] = [:],
-        body: Data? = nil
-    ) {
-        self.path = path
-        self.method = method
-        self.query = query
-        self.headers = headers
-        self.body = body
+  public init(
+    path: String,
+    method: HTTPMethod,
+    query: [URLQueryItem]?,
+    headers: [String: String] = [:],
+    body: Data? = nil
+  ) {
+    self.path = path
+    self.method = method
+    self.query = query
+    self.headers = headers
+    self.body = body
+  }
+
+  public func urlRequest(with url: URL) throws -> URLRequest {
+    guard
+      var components = URLComponents(
+        url: url.appendingPathComponent(path), resolvingAgainstBaseURL: true)
+    else {
+      throw URLError(.badURL)
     }
 
-    public func urlRequest(with url: URL) throws -> URLRequest {
-        guard var components = URLComponents(url: url.appendingPathComponent(path), resolvingAgainstBaseURL: true) else {
-            throw URLError(.badURL)
-        }
-
-        if let query = query, !query.isEmpty {
-            components.queryItems = components.queryItems ?? []
-            components.queryItems!.append(contentsOf: query)
-        }
-
-        guard let url = components.url else {
-            throw URLError(.badURL)
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
-        request.allHTTPHeaderFields = headers
-        request.httpBody = body
-
-        return request
+    if let query = query, !query.isEmpty {
+      components.queryItems = components.queryItems ?? []
+      components.queryItems!.append(contentsOf: query)
     }
+
+    guard let url = components.url else {
+      throw URLError(.badURL)
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    request.allHTTPHeaderFields = headers
+    request.httpBody = body
+
+    return request
+  }
 }
--- Tests/SimpleHTTPTests/SimpleHTTPTests.swift
+++ Tests/SimpleHTTPTests/SimpleHTTPTests.swift
@@ -1,11 +1,12 @@
-    import XCTest
-    @testable import SimpleHTTP
+import XCTest
 
-    final class SimpleHTTPTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-//            XCTAssertEqual(SimpleHTTP().text, "Hello, World!")
-        }
-    }
+@testable import SimpleHTTP
+
+final class SimpleHTTPTests: XCTestCase {
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    //            XCTAssertEqual(SimpleHTTP().text, "Hello, World!")
+  }
+}
